2b5a09815f547cdd3cb09b5809f9abcb9d98b7f8
lmm <myliming@gmail.com>
2024-04-16 21:20:44 +0800

    Added Upload interface

---
+++ src/main/java/org/ming/demo/UploadController.java
@@ -0,0 +1,34 @@
+package org.ming.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+public class UploadController {
+    private static final Logger log = LoggerFactory.getLogger(UploadController.class);
+
+    private final Path root = Paths.get("d:\\uploads");
+
+    @PostMapping("upload")
+    String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(),
+                    this.root.resolve(file.getOriginalFilename()));
+
+            return "success";
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return "fail";
+    }
+}
